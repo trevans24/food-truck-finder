07061fedbc7ec9d7d6370177894c548aaef5d2da
trevans24 <trevans24@gmail.com>
2017-05-08 21:30:27 -0600

    gulp working all css, js and html files in the public folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,22 @@ let gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	cleanCSS = require('gulp-clean-CSS'),
 	uglify = require('gulp-uglify'),
+	htmlmin = require('gulp-htmlmin'),
 	babel = require('gulp-babel');
 
 // DEFAULT GULP TASK
 
 gulp.task('default', function(){
 	console.log('i am a default task...');
-	gulp.watch('./public/sass/**/*.scss', ['styles']);
+	gulp.watch('src/sass/**/*.scss', ['styles']);
 	gulp.watch('src/**/*.js', ['front']);
+	gulp.watch('src/**/*.html', ['html']);
 });
 
 // GULP RUNNING CSS minified FILE FOR SASS
 
 gulp.task('styles', function(){
-	gulp.src('./public/sass/**/*.scss')
+	gulp.src('src/sass/**/*.scss')
 	.pipe(sass().on('error', sass.logError))
 	.pipe(cleanCSS())
 	.pipe(gulp.dest('./public/css/'));
@@ -34,3 +36,10 @@ gulp.task('front', function(){
 	.pipe(gulp.dest('./public/js/'));
 });
 
+// GULP RUNNING HTML minified
+
+gulp.task('html', function(){
+	return gulp.src('src/**/*.html')
+	.pipe(htmlmin({collapseWhitespace: true}))
+	.pipe(gulp.dest('./public/'));
+});
