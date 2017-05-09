b79d22d20089f51817651ca9f0635cc9b8317756
trevans24 <trevans24@gmail.com>
2017-05-09 15:39:54 -0600

    working on truck directive to use truck index.html

---
+++ src/directives/truckDirective.js
@@ -0,0 +1,15 @@
+// USE ANGULAR MODULE AND DIRECTIVE(S)
+angular.module('FoodTruckApp')
+	.directive('truckCard', truckCard);
+
+// TRUCK CARD DIRECTIVE
+	function truckCard(){
+		let directive = {
+			restrict: 'E',
+			replace: true,
+			templateUrl: '../templates/trucks/index.html',
+			scope: {
+				
+			}
+		}
+	}
