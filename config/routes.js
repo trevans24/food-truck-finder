9f6704591abfcb7228932169a627d2ce2ffde040
CoolCode2 <wcole2222@gmail.com>
2017-05-09 16:51:52 -0600

    added put route, fixed seed.js

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,7 @@ var trucksController = require('../controllers/trucksController.js');
 
 var Trucks = require('../models');
 
-
+/// TRUCKS ROUTES
 
 /// INDEX GET ALL
 router.get('/api/trucks', trucksController.index);
@@ -17,5 +17,8 @@ router.post('/api/trucks', trucksController.create);
 
 /// DESTROY 
 router.delete('/api/trucks/:id', trucksController.destroy);
+
+/// UPDATE
+router.put('/api/trucks/:id', trucksController.update);
 
 module.exports = router;
