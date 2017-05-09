6b60d8fc569faf5a6ee89cf4517cd307fdf7d14b
trevans24 <trevans24@gmail.com>
2017-05-08 19:54:03 -0600

    gulp working yayayayayayay

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,14 @@
-const gulp = require('gulp'),
-	sass = require('gulp-sass')
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	babel = require('gulp-babel');
+
+gulp.task('default', function(){
+	console.log('i am a default task...');
+	gulp.watch('./public/sass/**/*.scss', ['styles']);
+});
+
+gulp.task('styles', function(){
+	gulp.src('./public/sass/**/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('./public/css/'));
+});
