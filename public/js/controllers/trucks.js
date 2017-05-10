00cae9a626be4c0a1263bdfb6d0b8b21611a5662
trevans24 <trevans24@gmail.com>
2017-05-10 00:01:54 -0600

    trucks file now has all connected controllers in public, had to restart gulp to update it all

--- public/js/controllers/trucks.js
+++ public/js/controllers/trucks.js
@@ -1,16 +1,18 @@
 'use strict';
 
 console.log("Trucks Controller!!");
-// ANGULAR CONTROLLERS TO USE
+// ANGULAR CONTROLLERS TO USE, INDEX, SHOW, POST, PUT
 angular.module('FoodTruckApp').controller('TruckIndexController', TruckIndexController).controller('TruckShowController', TruckShowController).controller('TruckNewController', TruckNewController).controller('TruckEditController', TruckEditController);
 
 // INDEX CONTROLLER
 TruckIndexController.$inject = ['$http'];
-
+// Match Injection
 function TruckIndexController($http) {
+	// ViewModel is equal to this
 	var vm = this;
 	vm.deleteTruck = deleteTruck;
 
+	// INDEX all the Trucks
 	function getAllTrucks() {
 		console.log("GETTING All Trucks");
 		$http.get('/api/trucks').then(function (res) {
@@ -18,6 +20,7 @@ function TruckIndexController($http) {
 		});
 	}
 
+	//DELETE the trucks aka...clear the page
 	function deleteTrucks() {
 		console.log("DELETING All Trucks");
 		$http.delete('/api/trucks/' + truck.id).then(function (res) {
@@ -26,15 +29,18 @@ function TruckIndexController($http) {
 		});
 	}
 
+	// INDEX all the trucks after clearing the page
 	getAllTrucks();
 }
 
 // SHOW CONTROLLER
 TruckShowController.$inject = ['$http', '$routeParams'];
-
+// Match Injections
 function TruckShowController($http, $routeParams) {
+	// ViewModel is equal to this
 	var vm = this;
 
+	// GET the single truck
 	function getOneTruck() {
 		console.log($routeParams.id);
 		$http.get('/api/trucks/' + $routeParams.id).then(function (res) {
@@ -44,4 +50,51 @@ function TruckShowController($http, $routeParams) {
 	}
 
 	getOneTruck();
+}
+
+// NEW CONTROLLER
+TruckNewController.$inject = ['$http', '$location'];
+// Match Injections
+function TruckNewController($http, $location) {
+	// ViewModel is equal to this
+	var vm = this;
+	vm.saveTruck = saveTruck;
+
+	// POST the new truck added to the DB
+	function saveTruck() {
+		console.log(vm.newTruck);
+		$http.post('/api/trucks', vm.newTruck).then(function (res) {
+			var newTruck = res.data;
+			$location.path('/trucks/' + truck.id);
+		});
+	}
+}
+
+// PUT CONTROLLER
+TruckEditController.$inject = ['$http', '$location', '$routeParams'];
+// Match injections
+function TruckEditController($http, $location, $routeParams) {
+	// ViewModel is equal to this
+	var vm = this;
+	vm.updateTruck = updateTruck;
+
+	// GET the Truck to Update
+	function getTruck() {
+		console.log($routeParams.id);
+		$http.get('/api/trucks' + $routeParams.id).then(function (res) {
+			console.log(res);
+			vm.updatedTruck = res.data;
+		});
+	}
+
+	// PUT the Truck
+	function updateTruck() {
+		$http.put('/api/trucks/' + $routeParams.id, vm.updatedTruck).then(function (res) {
+			console.log(res);
+			var truck = res.data;
+			$location.path('/api/trucks' + truck.id);
+		});
+	}
+	// GET the Updated Truck
+	getTruck();
 }
