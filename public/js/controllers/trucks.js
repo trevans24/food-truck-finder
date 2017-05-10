11a219e166333000ebadcf1431d59f0ccb6b9dd5
trevans24 <trevans24@gmail.com>
2017-05-09 23:38:02 -0600

    finished writing the routes in the front end controller to match the back end controller

--- public/js/controllers/trucks.js
+++ public/js/controllers/trucks.js
@@ -2,10 +2,7 @@
 
 console.log("Trucks Controller!!");
 // ANGULAR CONTROLLERS TO USE
-angular.module('FoodTruckApp').controller('TruckIndexController', TruckIndexController);
-// .controller('TruckShowController', TruckShowController)
-// .controller('TruckNewController', TruckNewController)
-// .controller('TruckEditController', TruckEditController);
+angular.module('FoodTruckApp').controller('TruckIndexController', TruckIndexController).controller('TruckShowController', TruckShowController).controller('TruckNewController', TruckNewController).controller('TruckEditController', TruckEditController);
 
 // INDEX CONTROLLER
 TruckIndexController.$inject = ['$http'];
@@ -15,12 +12,14 @@ function TruckIndexController($http) {
 	vm.deleteTruck = deleteTruck;
 
 	function getAllTrucks() {
+		console.log("GETTING All Trucks");
 		$http.get('/api/trucks').then(function (res) {
 			vm.allTrucks = res.data;
 		});
 	}
 
 	function deleteTrucks() {
+		console.log("DELETING All Trucks");
 		$http.delete('/api/trucks/' + truck.id).then(function (res) {
 			var truckIndex = vm.allTrucks.indexOf(trucks);
 			vm.allTrucks.splice(truckIndex, 1);
@@ -28,4 +27,21 @@ function TruckIndexController($http) {
 	}
 
 	getAllTrucks();
+}
+
+// SHOW CONTROLLER
+TruckShowController.$inject = ['$http', '$routeParams'];
+
+function TruckShowController($http, $routeParams) {
+	var vm = this;
+
+	function getOneTruck() {
+		console.log($routeParams.id);
+		$http.get('/api/trucks/' + $routeParams.id).then(function (res) {
+			console.log(res);
+			vm.oneTruck = res.data;
+		});
+	}
+
+	getOneTruck();
 }
