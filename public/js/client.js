294601575e7739bc5bf7104fcf49924bb1f0bfe3
Troy Evans <troy@Troys-MacBook-Pro.local>
2017-05-11 17:31:29 -0600

    working home route

--- public/js/client.js
+++ public/js/client.js
@@ -1,84 +1,154 @@
 'use strict';
 
-console.log('Client Sided Controller');
+// console.log('Client Sided Controller');
 
-angular.module('FoodTruckApp', ['ngRoute', 'ui.router']).config(function ($routeProvider, $locationProvider, $stateProvier, $urlRouterProvider) {
+var app = angular.module('mapsApp', ['ngRoute']).controller('MapsController', MapsController).controller('TruckIndexController', TruckIndexController);
 
-	// using HTML 5 for location templates
-	$locationProvider.html5Mode({
-		enabled: true,
-		requireBase: false
-	});
+// using HTML 5 for location templates
 
-	// if no matched urls bring back to home
-	$urlRouterProvider.otherwise("/");
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+    // Main Routes
+    .when('/', {
+        templateUrl: '../templates/home.html',
+        contoroller: 'MapsController'
+    })
+    // ABOUT PAGE
+    .when('/about', {
+        templateUrl: '../templates/about.html'
+    })
 
-	$stateProvier
-	// Main Routes
-	// HOME
-	// .state('home', {
-	// 	url: '/',
-	// 	templateUrl: 'templates/index.html',
-	// 	controller: 'MainController',
-	// 	controllerAs: 'main'
-	// })
-	// ABOUT PAGE
-	.when('/about', {
-		templateUrl: '../templates/about.html'
-	})
+    // Truck Routes
+    // Index
+    .when('/trucks', {
+        templateUrl: '../templates/trucks/index.html',
+        controller: 'TruckIndexController'
+    })
 
-	// Truck Routes
+    // Show
+    .when('/trucks/:id', {
+        templateUrl: '../templates/trucks/show.html',
+        controller: 'TruckShowController'
+    })
 
-	// Index
-	.when('/trucks', {
-		templateUrl: '../templates/trucks/index.html',
-		controller: 'TruckIndexController as truckController'
-	})
+    // New
+    .when('/trucks', {
+        templateUrl: '../templates/trucks/new.html',
+        controller: 'TruckNewController'
+    })
 
-	// Show
-
-	.when('/trucks/:id', {
-		templateUrl: '../templates/trucks/show.html',
-		controller: 'TruckShowController as truckController'
-	})
-
-	// New
-	.when('/trucks', {
-		templateUrl: '../templates/trucks/new.html',
-		controller: 'TruckNewController as truckController'
-	})
-
-	// Edit
-	.when('/trucks/:id', {
-		templateUrl: '../templates/trucks/edit.html',
-		controller: 'TruckEditController as truckController'
-	});
-
-	// Favorites Routes
-
-	// Show Truck Ratings
-	// .state('/trucks/:id/favorite', {
-	// 	url: '',
-	// 	templateUrl: '../templates/favorties/show.html',
-	// 	controller: 'FavoriteShowController as favoriteController',
-	// 	controllerAs: ''
-	// })
-
-	// // New Rating
-	// .state('/trucks/:id/favorite/new', {
-	// 	url: '',
-	// 	templateUrl: '../templates/favorties/new.html',
-	// 	controller: 'FavoriteNewController as favoriteController',
-	// 	controllerAs: '',
-	// })
-
-	// // Edit Rating
-	// .state('/trucks/:id/favorite/:id', {
-	// 	url: '',
-	// 	templateUrl: '../templates/favorties/edit.html',
-	// 	controller: 'FavoriteEditController as favoriteController',
-	// 	controllerAs: '',
-	// 	resolve: {
-	// 		skipIfLoggedIn: skipIfLoggedIn
-	// 	}
+    // Edit
+    .when('/trucks/:id', {
+        templateUrl: '../templates/trucks/edit.html',
+        controller: 'TruckEditController'
+    });
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
 });
+
+console.log('Angular Working');
+// Controllers
+
+// INDEX CONTROLLER
+TruckIndexController.$inject = ['$http', '$scope'];
+// Match Injection
+function TruckIndexController($http, $scope) {
+    console.log('Index of Trucks');
+    $scope.hello = 'test';
+    $http.get('/api/trucks').then(function (res) {
+        console.log('you made it');
+    });
+}
+
+// MAPS CONTROLLER FOR TESTING
+MapsController.$inject = ['$scope', '$http'];
+function MapsController($scope, $http) {
+    var self = this;
+    $scope.trucks = [];
+    $scope.markers = [];
+    self.getTrucks = getTrucks;
+    self.filterTrucks = filterTrucks;
+    // Runs the initialize map function found in footer of index.html
+    initMap();
+    // Checks if browser is compatible with Geolocation
+    if (navigator.geolocation) {
+        console.log('Geolocation is supported!!');
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            console.log(pos.lat + " position");
+            userPos(pos);
+
+            function userPos(pos) {
+                var marker = new google.maps.Marker({
+                    map: $scope.map,
+                    position: new google.maps.LatLng(pos.lat, pos.lng)
+
+                });
+            }
+            // infoWindow.setPosition(pos);
+            //   infoWindow.setContent('Location found.');
+            //   infoWindow.open(map);
+            //   map.setCenter(pos);
+        });
+    } else {
+        console.log('Geolocation is not supported for this Browser/OS.');
+    }
+
+    // Gets trucks loaded into server
+    getTrucks();
+    // Grabs trucks from DB and runs createMarker to plot them on map
+    function getTrucks() {
+        $http.get("http://localhost:3000/api/trucks/").then(function (response) {
+            var trucks = response.data;
+            for (var i = 0; i < trucks.length; i++) {
+                createMarker(trucks[i]);
+                $scope.trucks.push(trucks);
+            }
+        });
+    }
+    // Allows user to filter the displayed results on the map
+    function filterTrucks(category) {
+        for (var i = 0; i < $scope.markers.length; i++) {
+            var marker = $scope.markers[i];
+            if (marker.category == category || category.length === 0) {
+                marker.setVisible(true);
+            } else {
+                marker.setVisible(false);
+            }
+        }
+    }
+    var mapOptions = {
+        zoom: 13,
+        center: new google.maps.LatLng(39.74, -104.99),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var infoWindow = new google.maps.InfoWindow();
+    var createMarker = function createMarker(info) {
+        var image = "../images/icons/foodTruck.png";
+        console.log("Creating  marker " + info);
+        console.log($scope.map);
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            icon: image,
+            position: new google.maps.LatLng(info.latitude, info.longitude),
+            title: info.name
+        });
+        marker.content = '<div class="infoWindowContent">' + 'Category: ' + info.food_type + '</div>' + '</br>' + '<a href="/trucks/' + info.id + '">Learn More</a>' + '</br>' + '<a href="https://www.google.com/maps/place/' + info.latitude + ',' + info.longitude + '&dirflg=w">Get Directions</a>';
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+            infoWindow.open($scope.map, marker);
+        });
+        $scope.markers.push(marker);
+    };
+    $scope.openInfoWindow = function (e, selectedMarker) {
+        e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
+    };
+}
