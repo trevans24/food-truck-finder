46c3a88bf528b59c9177bbe8bdff2b37a564c5ea
Troy Evans <troy@Troys-MacBook-Pro.local>
2017-05-11 11:39:59 -0600

    adding auth for satellizer on users in client js file

--- server.js
+++ server.js
@@ -16,8 +16,40 @@ app.use(trucksRouter); //pulls in config/routes.js
 
 app.use(express.static('public'));
 
-// CATCH ALL ROUTES
+// Routes for users authed through Satellizer
+// GET USER
+app.get('/api/me', auth.ensureAuthenticated, function(req,res) {
+	sqlUser.findById(req.user)
+	.then(function(user){
+		if(!user) {
+			return res.status(400)
+			.send({message: 'No user found'});
+		}
+		res.send(user);
+	});
+});
 
+// UPDATE USER
+app.put('/api/me', auth.ensureAuthenticated, function(req, res) {
+	sqlUser.findById(req.user)
+	.then(function(user){
+		if(!user) {
+			return res.status(400)
+			.send({message: 'No user found'});
+		}
+		user.email = req.body.email || user.email;
+		user.save()
+		.then(function(result){
+			if (!result) {
+				res.status(500)
+				.send({message: 'Update User Error'});
+			}
+			res.send(result);
+		});
+	});
+});
+
+// CATCH ALL ROUTES
 app.get(['/','/login','/signup','/logout'], function(req,res){
 	res.sendFile(__dirname + '/public/index.html');
 });
