d9b27ec30c663e251a6514f92d9d880f982751dd
CoolCode2 <wcole2222@gmail.com>
2017-05-12 11:59:57 -0600

    app can connect to heroku db

--- models/index.js
+++ models/index.js
@@ -1,14 +1,27 @@
 // SQL db =  'foodTrucks';
 var Sequelize = require('sequelize');
 
+
 // var sequelize = new Sequelize('postgres://alexiohearn@localhost:5432/foodtrucks');
-
 // var sequelize = new Sequelize('postgres://AllieG@localhost:5432/foodtrucks');
-var sequelize = new Sequelize('postgres://troy@localhost:5432/foodtrucks');
+// var sequelize = new Sequelize('postgres://troy@localhost:5432/foodtrucks');
 
-//var sequelize = new Sequelize('postgres://<username>@localhost:5432/tunr_models');
 
-// 
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+  // the application is executed on the local machine
+  sequelize = new Sequelize('postgres://AllieG@localhost:5432/foodtrucks');
+}
+
+// HEROKU: Username: znvzuadyvotpkn
+// HEROKU: DB_NAME: d1r1qjr0qvq8v9
 
 module.exports.Sequelize = Sequelize;
 module.exports.sequelize = sequelize;
@@ -25,3 +38,5 @@ module.exports.models = {
 	Trucks: Trucks,
 	Drivers: Drivers
 };
+
+//Created postgresql-flexible-86510 as DATABASE_URL
